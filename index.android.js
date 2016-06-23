74c92301f47e512f20d8e7bad5550c8b664a2544
elad <elad@localhost.localdomain>
2016-06-23 15:25:54 +0300

    Fixed scrollview

--- index.android.js
+++ index.android.js
@@ -4,49 +4,91 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Platform,
+    TouchableHighlight,
+    ScrollView
 } from 'react-native';
 
+import CinemaPreview from './App/cinema'
+
+var AppReloader = require('NativeModules').AppReloader;
+
 class SingleServe extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    handleSubmit() {
+        AppReloader.reloadAppWithURLString("http://localhost:8081/index.ios.bundle", "ShmerlingtonHotel", "ShmerlingtonHotel");
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.appBar}/>
+                <ScrollView style={styles.scrollview}>
+                    <View style={styles.insideScrollview}>
+                        <CinemaPreview/>
+                        <View style={styles.seperator}/>
+                        <CinemaPreview/>
+                        <View style={styles.seperator}/>
+                        <CinemaPreview/>
+                        <View style={styles.seperator}/>
+                        <CinemaPreview/>
+                        <View style={styles.seperator}/>
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
+const PURPLE = '#5C069A';
+const VERY_LIGHT_GREY = '#f5f5f5';
+const LIGHT_GREY = '#cccccc';
+const GREY = '#888';
+const MID_GREY = '#222';
+const DARK_GREY = '#444';
+
+const Colors = {
+    tintColor: PURPLE,
+    purple: PURPLE,
+    veryLightGrey: VERY_LIGHT_GREY,
+    lightGrey: LIGHT_GREY,
+    grey: GREY,
+    midGrey: MID_GREY,
+    darkGrey: DARK_GREY
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container:{
+        flex: 1
+    },
+    appBar: {
+        // position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 50,
+        bottom: 0,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // flex: 1,
+        backgroundColor: '#FFF'
+    },
+    scrollview: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+    insideScrollview: {
+        flex: 1
+    },
+    seperator: {
+        padding: 10
+    }
 });
 
 AppRegistry.registerComponent('SingleServe', () => SingleServe);
